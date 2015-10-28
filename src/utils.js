14a2ec5d41a7c4b08b0435435c714c8f0447fd2c
Stephen Mizell <smizell@gmail.com>
2015-10-28 08:32:24 -0500

    Fix linter

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,12 @@ export default {
   include: (array = [], item) => {
     return array.indexOf(item) >= 0;
   },
+
   isObject: (obj) => {
-    return obj !== null && typeof obj === 'object'
+    return obj !== null && typeof obj === 'object';
   },
+
   isArray: (array) => {
     return Array.isArray(array);
-  }
+  },
 };
