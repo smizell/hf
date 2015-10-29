bd81e96b4c682335f45714e1497c2ba17a2a6372
Stephen Mizell <smizell@gmail.com>
2015-10-29 08:23:09 -0500

    Add hasRel tests

---
+++ test/has-rel-test.js
@@ -0,0 +1,53 @@
+import {expect} from 'chai';
+import hf from '../src/index';
+
+describe('Hf #hasRel', () => {
+  const hfObj = {
+    transitions: [
+      {
+        tag: 'link',
+        rels: ['next'],
+        href: '/user/2',
+      },
+    ],
+  };
+
+  context('when the given rel exists', () => {
+    it('returns true', () => {
+      expect(hf.hasRel(hfObj, 'next')).to.be.true;
+    });
+  });
+
+  context('when the given rel does not exists', () => {
+    it('returns false', () => {
+      expect(hf.hasRel(hfObj, 'foobar')).to.be.false;
+    });
+  });
+
+  context('when given an Hf object', () => {
+    it('returns the `next` links', () => {
+      expect(hf.hasRel(hfObj, 'next')).to.be.true;
+    });
+  });
+
+  context('when given a transitions array', () => {
+    it('returns the `next` links', () => {
+      expect(hf.hasRel(hfObj.transitions, 'next')).to.be.true;
+    });
+  });
+
+  context('when the rel is not a string', () => {
+    const hfObjWithNumber = {
+      transitions: [
+        {
+          tag: 'link',
+          rels: [1],
+        },
+      ],
+    };
+
+    it('returns false', () => {
+      expect(hf.hasRel(hfObjWithNumber, 1)).to.be.false;
+    });
+  });
+});
