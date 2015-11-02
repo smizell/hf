5d6337673ff4764fca8c0072f529901004bb2ff5
Stephen Mizell <smizell@gmail.com>
2015-11-01 23:05:13 -0600

    Use lodash where possible

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import _ from './utils';
 export default {
   has: (value = {}, conditions = {}) => {
     if (!_.isObject(conditions)) return false;
-    if (_.isArray(value)) return _.has(value, conditions);
-    if (_.isObject(value)) return _.has(value.transitions, conditions);
+    if (_.isArray(value)) return !!_.find(value, conditions);
+    if (_.isObject(value)) return !!_.find(value.transitions, conditions);
     return false;
   },
 
@@ -44,35 +44,5 @@ export default {
     return obj.transitions || [];
   },
 
-  path: (obj = {}, steps = [], defaultValue) => {
-    if (!_.isObject(obj) && !_.isArray(obj)) return undefined;
-    if (!_.isArray(steps)) return undefined;
-    if (steps.length === 0 ) return undefined;
-
-    // Try to get the first step
-    const firstStep = obj[steps[0]];
-
-    // No need to continue if there isn't a match or if only one step
-    if (firstStep === undefined) {
-      return defaultValue;
-    } else if (firstStep && steps.length === 1) {
-      return firstStep;
-    }
-
-    // We've made it this far, so we have an initial value
-    let value = firstStep;
-
-    // Loop through the remainder of steps, hence starting at 1
-    for (let i = 1; i < steps.length; i++) {
-      if (value[steps[i]] !== undefined) {
-        value = value[steps[i]];
-      } else {
-        return undefined;
-      }
-    }
-
-    // Allows for returning null values
-    if (value !== undefined) return value;
-    return defaultValue;
-  },
+  path: _.path,
 };
--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,22 @@
+import filter from 'lodash.filter';
+import find from 'lodash.find';
 import isString from 'lodash.isstring';
 import isObject from 'lodash.isobject';
 import isArray from 'lodash.isarray';
 import includes from 'lodash.includes';
 import isMatch from 'lodash.ismatch';
+import path from 'lodash.get';
 
 export default {
+  find,
+  filter,
   includes,
   isObject,
   isArray,
   isString,
   isMatch,
+  path,
 
-  has: (transitions = [], conditions) => {
-    for (let i = 0; i < transitions.length; i++) {
-      if (isMatch(transitions[i], conditions)) return true;
-    }
-    return false;
-  },
-
-  getBy: (transitions = [], conditions) => {
-    for (let i = 0; i < transitions.length; i++) {
-      if (typeof (conditions) === 'function') {
-        if (conditions(transitions[i])) {
-          return transitions[i];
-        }
-      } else {
-        if (isMatch(transitions[i], conditions)) {
-          return transitions[i];
-        }
-      }
-    }
-  },
-
-  filterBy: (transitions = [], conditions) => {
-    if (typeof (conditions) === 'function') return transitions.filter(conditions);
-    return transitions.filter(transition => {
-      return isMatch(transition, conditions);
-    });
-  },
+  getBy: find,
+  filterBy: filter,
 };
